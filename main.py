934ff87b6a6f874b68a94f4098b455581e073a1a
itmanmax <121846009+itmanmax@users.noreply.github.com>
2024-10-26 16:44:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,12 @@ from pkg.plugin.host import EventContext, PluginHost
 from pkg.plugin.context import register, handler, llm_func, BasePlugin, APIHost, EventContext
 from pkg.plugin.events import *  # 导入事件类
 
-import requests
-
-from mirai import Image
 import logging
 import traceback
 
-
 # 注册插件
-@register(name="function", description="Hello function", version="0.1", author="max")
-class functionPlugin(BasePlugin):
-
-    # API URL
-    image_api_url = "http://www.maxtral.fun/APIphp/qchatgptfunction.php"
+@register(name="Fixedanswers", description="Hello Fixedanswers", version="0.2", author="max")
+class FixedanswersPlugin(BasePlugin):
 
     # 插件加载时触发
     def __init__(self, host: APIHost):
@@ -26,16 +19,21 @@ class functionPlugin(BasePlugin):
     async def _(self, event: EventContext):
         try:
             text = event.event.text_message
-            if text == "getfunction" or text == "功能":
+            
+            # 检测包含“最帅的人”的句子
+            if "最帅的人" in text:
                 event.prevent_default()
                 event.prevent_postorder()
-                # 发送图片
-                response = requests.get(self.image_api_url)
-                if response.ok:  # 使用 response.ok 来检查请求是否成功
-                    event.add_return("reply", [Image(url=response.url)])
-                else:
-                    event.add_return("reply", ["图片获取失败，请稍后再试。"])
-                logging.info("function!")
+                event.add_return("reply", ["最帅的人是max"])
+                logging.info("检测到最帅的人！")
+
+            # 检测包含“彭于晏的女朋友”的句子
+            elif "彭于晏的女朋友" in text:
+                event.prevent_default()
+                event.prevent_postorder()
+                event.add_return("reply", ["彭于晏的老婆是"])
+                logging.info("检测到彭于晏的女朋友！")
+
         except Exception as e:
             logging.error(traceback.format_exc())
 
